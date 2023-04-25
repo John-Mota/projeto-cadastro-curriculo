41ce0f7dd87fcae2af5a073fc3259c48a696ef5d
John Mota <johnmarcos.mota@gmail.com>
2023-04-25 10:28:06 -0300

    criação dos módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CandidatoComponent } from './candidato/candidato.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CandidatoComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule
