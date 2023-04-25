2ae30e1c5587eacc668c024de6b6b426467c6413
John Mota <johnmarcos.mota@gmail.com>
2023-04-25 13:26:18 -0300

    criação do template ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CandidatoComponent } from './candidato/candidato.component';
 import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CandidatoComponent,
-    AdminComponent
+    AdminComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
