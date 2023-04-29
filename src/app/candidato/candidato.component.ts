b8e866468cf81e8de2ac03abaf6e22d5a85c0e55
John Mota <johnmarcos.mota@gmail.com>
2023-04-29 00:11:04 -0300

    update

--- src/app/candidato/candidato.component.ts
+++ src/app/candidato/candidato.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validator } from '@angular/forms';
+import { FormGroup, FormBuilder, Validator, Validators, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-candidato',
@@ -8,18 +8,63 @@ import { FormGroup, FormBuilder, Validator } from '@angular/forms';
   styleUrls: ['./candidato.component.css']
 })
 export class CandidatoComponent implements OnInit{
-  candidato
+  candidatoForm!: FormGroup;
+
+  cadastroCriado: boolean = false;
+  exibirFormulario: boolean = true;
 
   constructor(private fb: FormBuilder ,private http: HttpClient) { }
 
-ngOnInit(): void {
-
-}
-
-
-    this.http.post('http://localhost:3000/curriculum', data).subscribe(response => {
-      console.log(response);
+  ngOnInit(): void {
+    this.candidatoForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      cpf: new FormControl('', Validators.required),
+      dataNascimento: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      escolaridade: new FormControl('', Validators.required),
+      funcao: new FormControl('', Validators.required),
+      competencias: new FormArray([
+        new FormGroup({
+          descricao: new FormControl('', Validators.required),
+          nivel: new FormControl('', Validators.required),
+        })
+      ])
     });
+    console.log(this.candidatoForm.valid)
+  }
+
+  get competencias() {
+    return this.candidatoForm.get('competencias') as FormArray;
+  }
+
+  adicionarCompetencia() {
+    this.competencias.push(new FormGroup({
+      descricao: new FormControl('', Validators.required),
+      nivel: new FormControl('', Validators.required),
+    }));
+  }
+
+
+
+
+  onSubmit() {
+    console.log(this.candidatoForm)
+    const data = this.candidatoForm.value;
+    data.competencias = this.competencias.getRawValue();
+    this.http.post('http://localhost:3000/curriculum', data)
+    .subscribe(response => {
+      this.cadastroCriado = true;
+      this.exibirFormulario = false;
+  });
+  }
+
+  voltarParaFormulario() {
+    this.cadastroCriado = false;
+    this.exibirFormulario = true;
+    this.candidatoForm.reset();
+  }
+
 
 
 }
